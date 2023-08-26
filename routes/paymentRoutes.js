c08172c97b0462783ec1e44cb54e38f443ef7cf0
unknown <ravikmr0031@gmail.com>
2023-08-26 12:35:22 -0700

    stripekey

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleWare/auth");
 const { processPayment, sendStripeApiKey } = require("../controllers/paymentController");
-// 
+
+// isAuthenticatedUser
+
 router.route("/payment/process").post( isAuthenticatedUser, processPayment )
-router.route("/stripeapikey").get( isAuthenticatedUser, sendStripeApiKey )
+router.route("/stripeapikey").get( sendStripeApiKey )
 
 
 
